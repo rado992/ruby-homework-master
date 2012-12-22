983ac38ff1d5391edeebba641532778a6f7948f3
Radoslav Varbanov <radvarbanov@abv.bg>
2012-12-22 14:36:37 +0200

    Original solution to Problem 1

---
+++ 01/solution.rb
@@ -0,0 +1,108 @@
+class Integer
+  def prime_divisors
+    div = []
+    num = self
+    num = num < 0 ? num * (-1) : num
+    2.upto(num) do |cur|
+      isprime = true
+      2.upto(cur - 1) do |check|
+        if (cur % check) == 0
+          isprime = false
+          break
+        end
+      end
+      if (num % cur) == 0 and isprime
+        div << cur
+      end
+      isprime = true
+    end
+    div
+  end
+end
+
+
+class Range
+  def fizzbuzz
+    rng = self
+    newrng = []
+    rng.each do |cur|
+      if (cur % 15) == 0
+        newrng[cur - 1] = :fizzbuzz
+      elsif (cur % 5) == 0
+        newrng[cur - 1] = :buzz
+      elsif (cur % 3) == 0
+        newrng[cur - 1] = :fizz
+      else
+        newrng[cur - 1] = cur
+      end
+    end
+    newrng
+  end
+end
+
+class Hash
+  def group_values
+    oldhash = self
+    newhash = Hash.new
+    curkeys = []
+    curval = 0
+    is_included = false
+
+    oldhash.each do |key, val|
+      curval = val
+
+      newhash.each do |nkey, nval|
+        if nval == val
+          is_included = true
+        end
+      end
+
+      oldhash.each do |tempkey, tempval|
+        if is_included == false and (curval == tempval)
+          curkeys << tempkey
+        end
+      end
+
+      newhash = newhash.merge({curval => curkeys})
+      curkeys = []
+      is_included = false
+    end
+    newhash
+  end
+end
+
+class Array
+  def densities
+    symbols = self
+    counts = []
+    cnt = 0
+    temp = 0
+    symbols.each do |sym|
+      cnt = cnt + 1
+    end
+    cnt = cnt - 1
+    0.upto cnt do |x|
+      0.upto cnt do |y|
+        if symbols[x] == symbols[y]
+          temp = temp + 1
+        end
+      end
+      counts << temp
+      temp = 0
+    end
+    counts
+  end
+end
+
+#p (1...60).fizzbuzz
+#p (1...60).fizzbuzz
+#p 4620.prime_divisors
+#p = { a: 4, b: 2, c: 1, d: 4 }
+#p p.group_values
+#p [:a, :b, :a, :baba, :a, :b].densities
+
+
+
+
+
+
